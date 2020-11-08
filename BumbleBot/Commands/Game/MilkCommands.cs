d64c099da8b9195a6a146cc09077d29c128129f1
Will <williamspires@hotmail.co.uk>
2020-11-08 18:10:09 +0000

    Add milk expiry lookup

--- BumbleBot/Commands/Game/MilkCommands.cs
+++ BumbleBot/Commands/Game/MilkCommands.cs
@@ -11,6 +11,10 @@ using MySql.Data.MySqlClient;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json;
+using System.Text;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Entities;
 
 namespace BumbleBot.Commands.Game
 {
@@ -45,6 +49,44 @@ namespace BumbleBot.Commands.Game
                     MilkingResponse milkingResponse = serializer.Deserialize<MilkingResponse>(jsonReader);
 
                     await ctx.Channel.SendMessageAsync(milkingResponse.message).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine(ex.Message);
+                Console.Out.WriteLine(ex.StackTrace);
+            }
+        }
+
+        [Command("expiry")]
+        public async Task CheckExpiry(CommandContext ctx)
+        {
+            try
+            {
+                string uri = $"http://localhost:8080/milk/expiry/{ctx.User.Id}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                List<ExpiryResponse> expiryDates = new List<ExpiryResponse>();
+                using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                {
+                    var jsonReader = new JsonTextReader(reader);
+                    JsonSerializer serializer = new JsonSerializer();
+                    expiryDates = serializer.Deserialize<List<ExpiryResponse>>(jsonReader);
+                }
+
+                if (expiryDates.Count > 0)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("Expiry dates are shown in the following format YYYY-MM-dd");
+                    expiryDates.ForEach((ExpiryResponse obj) => {
+                        sb.AppendLine($"{obj.milk} lbs of milk will expire at {obj.expiryDate}");
+                    });
+                    var interactivity = ctx.Client.GetInteractivity();
+                    var expiryPages = interactivity.GeneratePagesInEmbed(sb.ToString(), SplitType.Line, new DiscordEmbedBuilder());
+                    await interactivity.SendPaginatedMessageAsync(ctx.Channel, ctx.User, expiryPages)
+                        .ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
---
+++ BumbleBot/Models/ExpiryResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BumbleBot.Models
+{
+    public class ExpiryResponse
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+
+        [JsonProperty("discordID")]
+        public ulong discordID { get; set; }
+
+        [JsonProperty("milk")]
+        public decimal milk { get; set; }
+
+        [JsonProperty("expirydate")]
+        public String expiryDate { get; set; }
+    }
+}
