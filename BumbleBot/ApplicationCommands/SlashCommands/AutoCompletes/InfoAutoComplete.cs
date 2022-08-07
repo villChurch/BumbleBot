66897b920097bfcba0786d2c89d875f0ab239d57
Will <will@williamspires.com>
2022-08-07 09:02:18 +0100

    Add autocomplete and finish info command

---
+++ BumbleBot/ApplicationCommands/SlashCommands/AutoCompletes/InfoAutoComplete.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BumbleBot.Models;
+using BumbleBot.Utilities;
+using Dapper;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.Entities;
+using MySqlConnector;
+
+namespace BumbleBot.ApplicationCommands.SlashCommands.AutoCompletes
+{
+    public class InfoAutoComplete
+    {
+        private readonly DbUtils dbUtils = new();
+
+        public Task<IEnumerable<DiscordApplicationCommandAutocompleteChoice>> Provider(AutocompleteContext context)
+        {
+            var options = new List<DiscordApplicationCommandAutocompleteChoice>();
+            List<Info> infos;
+            using (var connection = new MySqlConnection(dbUtils.ReturnPopulatedConnectionString()))
+            {
+                connection.Open();
+                infos = connection.Query<Info>("select * from info").ToList();
+            }
+            infos.Where(i => i.Name.StartsWith(context.Options[0].Value?.ToString(), System.StringComparison.CurrentCultureIgnoreCase)).ToList().ForEach(delegate (Info info)
+            {
+                options.Add(new DiscordApplicationCommandAutocompleteChoice(info.Name, info.Name));
+            });
+            return Task.FromResult(options.AsEnumerable());
+        }
+    }
+}
--- BumbleBot/Commands/AdminCommands/InfoCommand.cs
+++ BumbleBot/Commands/AdminCommands/InfoCommand.cs
@@ -1,38 +1,133 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BumbleBot.Attributes;
 using BumbleBot.Models;
+using BumbleBot.Utilities;
 using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using BumbleBot.ApplicationCommands.SlashCommands.AutoCompletes;
+using DisCatSharp.Interactivity.Enums;
 using DisCatSharp.Interactivity.Extensions;
+using MySql.Data.MySqlClient;
+using Dapper;
 
 namespace BumbleBot.Commands.AdminCommands
 {
     public class InfoCommand : ApplicationCommandsModule
     {
+        private DbUtils dbUtils = new();
 
         [SlashCommand("info_add", "Add a new info prompt")]
         [OwnerOrPermissionSlash(DisCatSharp.Permissions.KickMembers)]
-        public async Task AddNewInfo(InteractionContext ctx, [Option("name", "name for info prompt")] string name)
+        public async Task AddNewInfo(InteractionContext ctx)
         {
             var id = RandomID(8);
-            await ctx.CreateModalResponseAsync(new DisCatSharp.Entities.DiscordInteractionModalBuilder()
+            var nameId = RandomID(8);
+            var valueId = RandomID(8);
+            var modal = await ctx.Interaction.CreatePaginatedModalResponseAsync(
+                new List<ModalPage>()
+                {
+                    new ModalPage(new DisCatSharp.Entities.DiscordInteractionModalBuilder()
                 .WithCustomId(id)
                 .WithTitle("Info prompt to add")
                 .AddModalComponents(new DisCatSharp.Entities.DiscordTextComponent(DisCatSharp.Enums.TextComponentStyle.Small,
-                RandomID(8),"Name"))
+                nameId,"Name"))
                 .AddModalComponents(new DisCatSharp.Entities.DiscordTextComponent(DisCatSharp.Enums.TextComponentStyle.Paragraph,
-                RandomID(8), "Value")));
-            var interactivity = ctx.Client.GetInteractivity();
-            var result = await interactivity.WaitForModalAsync(id);
-            if (result.TimedOut)
+                valueId, "Value")))
+                });
+
+            if (modal.TimedOut)
             {
-                await ctx.EditResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                await modal.Interaction.EditOriginalResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
                     .WithContent("Interaction has timed out"));
             }
             else
             {
-                var values = result.Result;
+                var info = new Info()
+                {
+                    Name = modal.Responses[nameId],
+                    Value = modal.Responses[valueId]
+                };
+                await using (var connection = new MySqlConnection(dbUtils.ReturnPopulatedConnectionString()))
+                {
+                    try
+                    {
+                        const string query = "Insert into info (name, value) VALUES (?name, ?value)";
+                        await connection.OpenAsync();
+                        var command = new MySqlCommand(query, connection);
+                        command.Parameters.AddWithValue("?name", info.Name);
+                        command.Parameters.AddWithValue("?value", info.Value);
+                        await command.ExecuteNonQueryAsync();
+                        await modal.Interaction.EditOriginalResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                            .WithContent($"Created info command {info.Name}."));
+                    } catch (MySqlException mse)
+                    {
+                        await modal.Interaction.EditOriginalResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                            .WithContent($"Info command already exists for {info.Name}"));
+                    }
+                    finally
+                    {
+                        await connection.CloseAsync();
+                    }
+                }
+            }
+        }
+
+        [SlashCommand("info", "Display information on a topic")]
+        public async Task DisplayInfo(InteractionContext ctx, [Autocomplete(typeof(InfoAutoComplete))]
+            [Option("name", "Name of Info command to show", true)] string name)
+        {
+            await ctx.CreateResponseAsync(DisCatSharp.InteractionResponseType.DeferredChannelMessageWithSource);
+            List<Info> infos;
+            using (var connection = new MySqlConnection(dbUtils.ReturnPopulatedConnectionString()))
+            {
+                connection.Open();
+                infos = connection.Query<Info>("select * from info").ToList();
+            }
+            if (infos.Count < 1)
+            {
+                await ctx.EditResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                    .WithContent($"No info value for {name}"));
+            }
+            else
+            {
+                var info = infos.Where(i => i.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+                await ctx.EditResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                    .WithContent(info.First().Value));
+            }
+        }
+
+        [SlashCommand("info_delete", "Delete an info prompt")]
+        public async Task DeleteInfo(InteractionContext ctx, [Autocomplete(typeof(InfoAutoComplete))]
+            [Option("name", "Name of Info command to delete", true)] string name)
+        {
+            await ctx.CreateResponseAsync(DisCatSharp.InteractionResponseType.DeferredChannelMessageWithSource);
+            List<Info> infos;
+            using (var connection = new MySqlConnection(dbUtils.ReturnPopulatedConnectionString()))
+            {
+                connection.Open();
+                infos = connection.Query<Info>("select * from info").ToList();
+            }
+            if (infos.Count < 1)
+            {
+                await ctx.EditResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                    .WithContent($"No info value for {name}"));
+            }
+            else
+            {
+                using (var connection = new MySqlConnection(dbUtils.ReturnPopulatedConnectionString()))
+                {
+                    var query = "delete from info where name = ?name";
+                    connection.Open();
+                    var command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("?name", name);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                await ctx.EditResponseAsync(new DisCatSharp.Entities.DiscordWebhookBuilder()
+                    .WithContent($"Deleted info prompt {name}"));
             }
         }
 
