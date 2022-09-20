d670558b8292f5bfe3076b01d8ff4443ad4fd143
Will Spires <will@williamspires.com>
2022-09-20 21:20:11 +0100

    initial commit of welcome message feature

---
+++ BumbleBot/Utilities/ModalHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace BumbleBot.Utilities
+{
+    public class ModalHelper
+    {
+        public String RandomID(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
+
